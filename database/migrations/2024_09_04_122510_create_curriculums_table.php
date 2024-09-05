51d71dcdf5ea3ea90d5c132f150b212b3c14cb08
obeco <hutamame@iCloud.com>
2024-09-05 13:56:22 +0900

    curriculums/delivery_times migrationファイル追加

---
+++ database/migrations/2024_09_04_122510_create_curriculums_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('thumbnail')->nullable();
+            $table->longText('description')->nullable();
+            $table->mediumText('video_url')->nullable();
+            $table->tinyInteger('always_delivery_flg');
+            // 外部キー制約
+            $table->integer('grade_id')->unsigned();
+            $table->foreign('grade_id')->references('id')->on('grades');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculums');
+    }
+};
