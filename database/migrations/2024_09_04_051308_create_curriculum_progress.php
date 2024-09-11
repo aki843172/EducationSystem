7b542c4a8afad687908816cd14b2b12e22fd47c1
aki843172 <aki843172@gmail.com>
2024-09-11 23:25:48 +0900

    Add userstable and curriculum progress migrations

--- database/migrations/2024_09_04_051308_create_curriculum_progress.php
+++ database/migrations/2024_09_04_051308_create_curriculum_progress.php
@@ -13,17 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('curriculum_progress', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('name_kana');
-            $table->string('email');
-            $table->string('password');
-            $table->string('profile_image')->nullable();
-            $table->unsignedBigInteger('grade_id');
+            $table->unsigneBigInteger('curriculumus_id');
+            $table->unsigneBigInteger('user_id');
+            $table->tinyInteger('clear_flg')->default(0);
             $table->timestamps();
 
-            
+             
         });
     }
 
@@ -34,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('curriculum_progress');
     }
 };
